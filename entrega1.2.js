738d6f46edcd977d01c77556c4bae63858a4df88
metawiser <metawiser@gmail.com>
2023-02-22 13:34:43 +0100

    ejercicios actualizado

--- entrega1.2.js
+++ entrega1.2.js
@@ -1,34 +1,52 @@
 /** NIVEL 1 EJERCICIO 1 */
 // Arrow Function autoinvocable que muestra la suma de dos numeros
-const devolverObjeto = ((a, b) => {
-    console.log(a + b); 
-})(17,6);
+console.log(((a, b) => a + b)(17,6));
 
 /** ********************************************************************** */
 /** NIVEL 2 EJERCICIO 1 */
-// Arrow Function que retorna un Objeto con un Atributo que se ha pasado como Parametro
-const anadirParametroAlObjeto = (atributo) => {
-        return {parametro : atributo}
-};
-
+// Arrow Function que retorna un Objeto con un Atributo que tiene un valor al Parametro recibido
+const anadirParametroAlObjeto = valor => ( {curso : valor} );
 console.log(anadirParametroAlObjeto('Node.js'));
 
 /** ********************************************************************** */
 /** NIVEL 2 EJERCICIO 2 */
 //Creo la Clase PERSONA
-class PERSONA {
+class Persona {
     constructor (nom) {
         this.nom = nom
     }
     //Metodo que muestra el nom 
-    dirNom = () => console.log(this.nom);
+    dirNom() {console.log(this.nom);} 
 }
 // Creo instancia de PERSONA
-const persona = new PERSONA('Carlos Z.');
+const persona = new Persona('Carlos Z.');
 // Invoco el Metodo para mostrar el nombre por la consola 
 persona.dirNom();
+
+console.log(Persona.prototype.dirNom);
+
 /** ************************************************************************ */
 /** NIVEL 3 EJERCICIO 1 */
-const creadorDeObjetos = () => {
-    
+
+class Curso {
+    contenido(){throw new error('no se puede invocar metodos abstractos');}
 }
+class NodeJs extends Curso {
+    verContenido(){ return 'contenido de Node.js disponile';}
+}
+class Javascript extends Curso {
+    verContenido(){ return 'contenido de Javascript disponible';}
+}
+const crearCurso = (curso) => {
+    if(curso === 'NodeJs') {
+        return new NodeJs()
+    } else if(curso === 'Javascript'){
+        return new Javascript()
+    } else {
+        throw new error('Ese curso no existe')
+    }
+}
+
+const nuevoCursoDeNodejs = crearCurso('NodeJs')
+console.log(nuevoCursoDeNodejs);
+
