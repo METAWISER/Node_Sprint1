6e57b1a43b23b43be41da82adf5d2a4c268cb240
Carlos <metawiser@gmail.com>
2023-02-25 15:12:48 +0100

    Ultimos ejercicios del nivel 2 añadidos

--- entrega1.3.js
+++ entrega1.3.js
@@ -5,11 +5,13 @@ function promesa(){
     // Retornamos nueva promesa
     return new Promise((resolve, reject) => {
         const numerosRandom = Math.floor(Math.random() * 10)
-        if(numerosRandom < 5){
-            resolve(`El numero ${numerosRandom} es menor que 5.`)
-        } else {
-            reject(`El numero ${numerosRandom} es igual o mayor que 5.`)
-        }  
+        setTimeout(() => {
+            if(numerosRandom < 5){
+                resolve(`El numero ${numerosRandom} es menor que 5.`)
+            } else {
+                reject(`El numero ${numerosRandom} es igual o mayor que 5.`)
+            }  
+        }, 100);
     })  
 }
 
@@ -25,7 +27,7 @@ promesa()
     })
 
 /** NIVEL 1 EJERCICIO 2 */
-console.log(`\n ---NIVEL 1 EJERCICIO 2---`);
+console.log(` \n ---NIVEL 1 EJERCICIO 2--- `);
 // Arrow Fuction que recibe un parametro y una funcion de callback 
 const arrowFuntion = (numero, callback) => {
         if(numero > 0) {
@@ -41,3 +43,86 @@ arrowFuntion(0, (mensaje) => {
 
 
 /** *********************************************************************** */
+/** NIVEL 2 EJERCICIO 1 */
+//Objetos copiados del Nivel 2 Ejercicio 1
+let employees = [{
+    id: 1,
+    name: 'Linux Torvalds'
+}, {
+    id: 2,
+    name: 'Bill Gates'
+},{
+    id: 3,
+    name: 'Jeff Bezos'
+}];
+ 
+let salaries = [{
+    id: 1,
+    salary: 4000
+}, {
+    id: 2,
+    salary: 1000
+}, {
+    id: 3,
+    salary: 2000
+}];
+
+const getEmployee = (id) => {
+    return new Promise((resolve, reject)=>{
+        const empleado = employees.find((emp)=> emp.id === id )
+        if(empleado){
+            resolve(empleado.name);
+        } else {
+            reject(new Error(`No se ha encontrado el empleado con ID ${id}`));
+        }   
+    });
+};
+getEmployee(1)
+    .then((empleado) => {
+        console.log(`\n ---NIVEL 2 EJERCICIO 1---`);
+        console.log(empleado);
+    }) .catch((error) =>{
+        console.log(`\n ---NIVEL 2 EJERCICIO 1---`);
+        console.log(error);
+    })
+
+
+/** NIVEL 2 EJERCICIO 2 */
+//Funcion que recibe un objecto como parametro y devuelve su salario
+const getSalary = (employee) => {
+    return new Promise((resolve, reject) => {
+        const salario = salaries.find((salario) => salario.id === employee.id);
+        if (salario) {
+            resolve(salario.salary);
+        } else {
+            reject(`No se encontró el salario para el empleado con id ${employee.id}`);
+        }
+    });
+};
+// Creo un objeto para pasarle como parametro a la funcion getSalary()
+const empleadoUno = {id: 1, name: 'Linux Torvalds'};
+// llamo la funcion que retorna el Salario del 
+getSalary(empleadoUno)
+    .then(salario => {
+        console.log(`\n ---NIVEL 2 EJERCICIO 2---`);
+        console.log(salario)})
+    .catch(error => {
+        console.log(`\n ---NIVEL 2 EJERCICIO 2---`);
+        console.log(error)});
+
+
+/** NIVEL 2 EJERCICIO 3 */
+// Invoco las funciones getEmployee seguido de getSalary anidando la ejecucion de las dos promesas devolviendo el empleado y el salario 
+getEmployee(6)
+    .then((empleado) => {
+        console.log(`\n ---NIVEL 2 EJERCICIO 3---`);
+        console.log(`Empleado: ${empleado}`);
+        return getSalary(empleadoUno)
+    })
+    .then((salario) => {
+        console.log(`Salario: ${salario}`);
+    })
+    .catch((error) => {
+        console.log(`\n ---NIVEL 2 EJERCICIO 2---`);
+        console.log(error);
+    });
