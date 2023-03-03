f11a2b77ba2db0f03bd29e4bcbfedbb733b86c9a
Carlos <metawiser@gmail.com>
2023-03-03 15:57:26 +0100

    Creando el README.md

--- README.md
+++ README.md
@@ -1,1 +1,154 @@
-# Node_Sprint1
+# Entrega 1.5
+
+**Tabla de Contenido**
+
+[TOCM]
+
+
+## Prerrequisitos
+Antes de ejecutar el código es necesario tener instalado Node.js en tu sistema operativo.
+
+## Ejecución de las funciones
+El código consta de tres niveles de ejercicios, cada uno con distintas funciones que se ejecutan de manera independiente. Debes ejecutar el archivo **entrega1.5.js** en la consola usando el siguiente comando:
+`$ node entrega1.5.js` 
+
+### Nivel 1
+#### Ejercicio 1
+```
+fs.appendFile("pruebaEjercicio1.5.txt", `Hola`, (err) => {
+  if (err) throw err;
+});
+```
+Esta función escribe el texto 'Hola' en un archivo llamado **pruebaEjercicio1.5.txt**.
+
+#### Ejercicio 2
+```
+fs.readFile("pruebaEjercicio1.5.txt", "utf8", (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`${data}`);
+  }
+});
+```
+Esta función lee el contenido del archivo **pruebaEjercicio1.5.txt** y lo muestra por consola.
+
+#### Ejercicio 3
+```
+const comprimirArchivo = (entrada, salida) => {
+  const documetoEntrada = fs.createReadStream(entrada);
+  const documentoSalida = fs.createWriteStream(salida);
+  try {
+    documetoEntrada.pipe(gzip).pipe(documentoSalida);
+  } catch (error) {
+    console.log(error);
+  }
+};
+```
+Esta función comprime el archivo **pruebaEjercicio1.5.txt** y lo guarda como **pruebaEjercicio1.5.txt.gz**.
+
+### Nivel 2
+#### Ejercicio 1
+```
+let contar = 0;
+const funcionRecusiva = setInterval(() => {
+  contar++;
+  if (contar === 1) {
+    clearInterval(funcionRecusiva);
+  }
+}, 1000);
+ ```
+Esta función imprime un mensaje de forma recursiva por consola cada segundo.
+
+#### Ejercicio 2
+```
+const archivoUsuario = "dir C:\\Users";
+  exec(archivoUsuario, (error, stdout, stderr) => {
+    if (error) {
+      console.log(`Hubo un error: ${error}`);
+    }
+    if (stderr) {
+     console.log(stderr);
+    }
+  console.log(stdout);
+});
+```
+Esta función lista el contenido del directorio de usuario en Windows y lo muestra por consola.
+
+### Nivel 3
+#### Ejercicio 1
+```
+function crearArchivoCodificado() {
+  // leer el archivo
+  const data = fs.readFileSync("pruebaEjercicio1.5.txt", "utf-8");
+  // codificar en hexadecimal
+  const hexData = data.toString("hex");
+  const hexFile = "pruebaEjercicio1.5_hex.txt";
+  fs.writeFileSync(hexFile, hexData);
+
+  // codificar en base64
+  const base64Data = Buffer.from(data).toString("base64");
+  const base64File = "pruebaEjercicio1.5_base64.txt";
+  fs.writeFileSync(base64File, base64Data);
+
+  console.log(`Archivos creados: ${hexFile}, ${base64File}`);
+}
+```
+Esta función codifica el archivo **pruebaEjercicio1.5.txt** en hexadecimal y en base64 y guarda los archivos codificados como **pruebaEjercicio1.5_hex.txt** y **pruebaEjercicio1.5_base64.txt**
+
+#### Ejercicio 2
+```
+const clave = scryptSync("1234", "salt", 24);
+
+function cifrarArchivo(ruta_archivo, ruta_archivo_salida, clave) {
+  // Leo el archivo de entrada y creo el de salida
+  const entrada = fs.createReadStream(ruta_archivo);
+  const salida = fs.createWriteStream(ruta_archivo_salida);
+  // Emcripto los datos
+  const iv = randomBytes(16);
+  const cipher = createCipheriv("aes-192-cbc", clave, iv);
+  let archivoEncriptado;
+  entrada.on("data", (data) => {
+    archivoEncriptado = cipher.update(data);
+    //Guardo los datos encriptados en el nuevo archivo
+    salida.write(archivoEncriptado);
+  });
+  entrada.on("end", () => {
+    salida.end();
+    //Borro el archivo ruta_archivo
+    fs.unlinkSync(ruta_archivo);
+  });
+}
+```
+Esta función encripta el archivo **pruebaEjercicio1.5_hex.txt** y guarda el archivo encriptado como **pruebaEjercicio1.5.enc**, por ultimo elimina el archivo **pruebaEjercicio1.5_hex.txt**.
+
+#### Ejercicio 3
+```
+const descifrarArchivos = (ruta_archivo, ruta_archivo_salida, clave) => {
+  // Leo el archivo de entrada y creo el de salida
+  const entrada = fs.createReadStream(ruta_archivo);
+  const salida = fs.createWriteStream(ruta_archivo_salida);
+  //Encrypto los datos
+  const iv = randomBytes(16);
+  const decipher = createDecipheriv("aes-192-cbc", clave, iv);
+  let descifrado;
+  entrada.on("data", (data) => {
+    descifrado = decipher.update(data);
+    salida.write(descifrado);
+  });
+  entrada.on("end", () => {
+    salida.end();
+  });
+};
+
+```
+Esta función desencripta el archivo pruebaEjercicio1.5.enc y lo guarda como pruebaEjercicio1.5_hex.txt. 
+
+
+**Tabla de Contenido**
+
+[TOCM]
+
+
+
+### FIN
--- entrega1.5.js
+++ entrega1.5.js
@@ -1,89 +1,143 @@
-const fs = require('fs');
-const fsPromise = require('fs')
-const zlib = require('zlib');
-const { exec } = require('child_process');
-const { stdout, stderr } = require('process');
-const crypto = require('crypto')
-const {path} = require('path')
+const fs = require("fs");
+const zlib = require("zlib");
+const { exec } = require("child_process");
+const { stdout, stderr } = require("process");
+const {
+  scryptSync,
+  createCipheriv,
+  createDecipheriv,
+  randomBytes,
+} = require("crypto");
+const { path } = require("path");
 const gzip = zlib.createGzip();
-
 
 /**NIVEL 1 */
 //Funcion que escribe un mensaje en un archivo
-fs.appendFile('pruebaEjercicio1.5.txt',`Hola`, (err) => {
-    if(err) throw err;
-    console.log('\n NIVEL 1 EJERCICIO 1 \n *****Mensaje añadido correctamente***** \n');
+fs.appendFile("pruebaEjercicio1.5.txt", `Hola`, (err) => {
+  if (err) throw err;
+  console.log(
+    "\n NIVEL 1 EJERCICIO 1 \n *****Mensaje añadido correctamente***** \n"
+  );
 });
 // Funcion que muestra por la consola el contenido añadido en la funcion anterior
-fs.readFile('pruebaEjercicio1.5.txt', 'utf8', (err, data) => {
-    if(err){
-        console.log(err);
-    } else {
-        console.log(`NIVEL 1 EJERCICIO 2 \n ${data}`);
-    }
+fs.readFile("pruebaEjercicio1.5.txt", "utf8", (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`NIVEL 1 EJERCICIO 2 \n ${data}`);
+  }
 });
-
 
 // Funcion que comprima el archivo que estamos usando para el ejercicio
 const comprimirArchivo = (entrada, salida) => {
-    const documetoEntrada = fs.createReadStream(entrada);
-    const documentoSalida = fs.createWriteStream(salida); 
-    try {
-        console.log(`\n NIVEL 1 EJERCICIO 3 \n El archivo ${entrada} se ha compirmido correctamente`);
-        documetoEntrada.pipe(gzip).pipe(documentoSalida) 
-    } catch (error) {
-        console.log(error);
-    }
-}
-comprimirArchivo('pruebaEjercicio1.5.txt', 'pruebaEjercicio1.5.txt.gz')
-
+  const documetoEntrada = fs.createReadStream(entrada);
+  const documentoSalida = fs.createWriteStream(salida);
+  try {
+    console.log(
+      `\n NIVEL 1 EJERCICIO 3 \n El archivo ${entrada} se ha compirmido correctamente`
+    );
+    documetoEntrada.pipe(gzip).pipe(documentoSalida);
+  } catch (error) {
+    console.log(error);
+  }
+};
+comprimirArchivo("pruebaEjercicio1.5.txt", "pruebaEjercicio1.5.txt.gz");
 
 /** ****************************************************************************************** */
 /** NIVEL 2 */
 //Funcion recursiva que imprime un mensaje por la consola cada segundo
 let contar = 0;
 const funcionRecusiva = setInterval(() => {
-    console.log(`NIVEL 2 EJERCICIO 1 \n`);
-    // le he puesto el contador en 1 para detener el bucle y ver solo un console.log()
-    contar++;
-    if (contar === 1){
-        clearInterval(funcionRecusiva)
-    }
+  console.log(`NIVEL 2 EJERCICIO 1 \n`);
+  // le he puesto el contador en 1 para detener el bucle y ver solo un console.log()
+  contar++;
+  if (contar === 1) {
+    clearInterval(funcionRecusiva);
+  }
 }, 1000);
 
-
-
 //Funcion que lista por la consola el contenido del directorio de usuario
-const archivoUsuario = 'dir C:\\Users'
+const archivoUsuario = "dir C:\\Users";
 
-exec(archivoUsuario, (error, stdout, stderr) =>{
-    if (error) {
-        console.log(`Hubo un error: ${error}`);
-    }
-    if (stderr) {
-        console.log(stderr);
-    }
-    console.log(`\n NIVEL 2 EJERCICIO 2 \n ${stdout}`);
-})
+exec(archivoUsuario, (error, stdout, stderr) => {
+  if (error) {
+    console.log(`Hubo un error: ${error}`);
+  }
+  if (stderr) {
+    console.log(stderr);
+  }
+  console.log(`\n NIVEL 2 EJERCICIO 2 \n ${stdout}`);
+});
 
 /** ********************************************************************************************* */
 /** NIVEL 3 */
 // Funcion que codifica dos archivos, uno en hexadecimal y otro en base64
 function crearArchivoCodificado() {
   // leer el archivo
-  const data = fs.readFileSync('pruebaEjercicio1.5.txt', 'utf-8');
+  const data = fs.readFileSync("pruebaEjercicio1.5.txt", "utf-8");
   // codificar en hexadecimal
-  const hexData = data.toString('hex');
-  const hexFile = 'pruebaEjercicio1.5_hex.txt';
+  const hexData = data.toString("hex");
+  const hexFile = "pruebaEjercicio1.5_hex.txt";
   fs.writeFileSync(hexFile, hexData);
 
   // codificar en base64
-  const base64Data = Buffer.from(data).toString('base64');
-  const base64File = 'pruebaEjercicio1.5_base64.txt';
+  const base64Data = Buffer.from(data).toString("base64");
+  const base64File = "pruebaEjercicio1.5_base64.txt";
   fs.writeFileSync(base64File, base64Data);
 
-  console.log(`Archivos creados: ${hexFile}, ${base64File}`);
+  console.log(`\n NIVEL 3 EJERCICIO 1 \n Archivos creados: ${hexFile}, ${base64File}`);
+}
+crearArchivoCodificado();
+
+const clave = scryptSync("1234", "salt", 24);
+//Funcion que encrypta un archivo
+function cifrarArchivo(ruta_archivo, ruta_archivo_salida, clave) {
+  // Leo el archivo de entrada y creo el de salida
+  const entrada = fs.createReadStream(ruta_archivo);
+  const salida = fs.createWriteStream(ruta_archivo_salida);
+  // Emcripto los datos
+  const iv = randomBytes(16);
+  const cipher = createCipheriv("aes-192-cbc", clave, iv);
+  let archivoEncriptado;
+  entrada.on("data", (data) => {
+    archivoEncriptado = cipher.update(data);
+    //Guardo los datos encriptados en el nuevo archivo
+    salida.write(archivoEncriptado);
+  });
+  entrada.on("end", () => {
+    salida.end();
+    //Borro el archivo ruta_archivo
+    fs.unlinkSync(ruta_archivo);
+    console.log(`\n NIVEL 3 EJERCICIO 2 \n El archivo ${ruta_archivo} ha sido cifrado y eliminado.`);
+  });
 }
 
-crearArchivoCodificado()
+cifrarArchivo("./pruebaEjercicio1.5_hex.txt", "pruebaEjercicio1.5.enc", clave);
 
+
+const descifrarArchivos = (ruta_archivo, ruta_archivo_salida, clave) => {
+  // Leo el archivo de entrada y creo el de salida
+  const entrada = fs.createReadStream(ruta_archivo);
+  const salida = fs.createWriteStream(ruta_archivo_salida);
+  //Encrypto los datos
+  const iv = randomBytes(16);
+  const decipher = createDecipheriv("aes-192-cbc", clave, iv);
+  let descifrado;
+  entrada.on("data", (data) => {
+    descifrado = decipher.update(data);
+    salida.write(descifrado);
+  });
+  entrada.on("end", () => {
+    salida.end();
+    console.log(`\n NIVEL 3 EJERCICIO 3 \n El Archivo ${ruta_archivo} ha sido descifrado`);
+  });
+};
+
+// Invoco la funcion con un setTimeout para esperar que el archivo encriptado './pruebaEjercicio1.5.enc' se cree y asi utilizarlo en la funcion y que no genere un error por que no se encuentre el archivo.
+  setTimeout(() => {
+    descifrarArchivos(
+        "./pruebaEjercicio1.5.enc",
+        "pruebaEjercicio1.5._hex.txt",
+        clave
+      )
+  }, 2000);
